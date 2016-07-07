56f4549526697d93a9a766841b88545da87a6887
Anastasiya Zapariy <a.zapary@rambler-co.ru>
2016-07-07 18:13:27 +0300

    add CommentList component

--- src/CommentList.js
+++ src/CommentList.js
@@ -1,3 +1,39 @@
-/**
- * Created by azapary on 07.07.16.
- */
+import React, { Component } from 'react'
+import Comment from './Comment'
+
+
+class CommentList extends Component {
+    state = {
+        isOpen: false
+    };
+    
+    render() {
+        const comments = this.props.comments;
+
+        if (!comments) return (null);
+
+        const { isOpen } = this.state;
+        const button = <a href="#" onClick= {this.toggleOpen}> {isOpen ? 'Close comments ' : 'Open comments '} [{comments.length}]</a>;
+
+        const listItems = (isOpen && comments.length)? comments.map((comment) => <Comment comment = {comment} key = {comment.id}/>) : null;
+
+        return (
+            <section>
+                {button}
+                <ul>
+                    {listItems}
+                </ul>
+            </section>
+
+        )
+    }
+    toggleOpen = (ev) => {
+        ev.preventDefault;
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
+}
+
+
+export default CommentList
