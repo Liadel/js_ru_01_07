bf3ccd7a3917322b22cbd8687e548e5a8cf01bd3
Anastasiya Zapariy <a.zapary@rambler-co.ru>
2016-07-11 19:51:31 +0300

    add decorator for only 1 article open at once

--- src/ArticleList.js
+++ src/ArticleList.js
@@ -1,27 +1,22 @@
 import React, { Component }  from 'react'
 import Article from './Article'
+import openArticle from './decorators/openArticle'
 
 class ArticleList extends Component {
     state = {
         openArticleId: null
-    }
+    };
 
-    openArticle = id => ev => {
-        if (ev) ev.preventDefault()
-        this.setState({
-            openArticleId: id
-        })
-    }
 
     render() {
-        const { articles } = this.props
+        const { articles, openArticleId, openArticle } = this.props;
 
         const listItems = articles.map((article) => <li key={article.id}>
             <Article article = {article}
-                isOpen = {article.id == this.state.openArticleId}
-                openArticle = {this.openArticle(article.id)}
+                isOpen = {article.id == openArticleId}
+                openArticle = {openArticle(article.id)}
             />
-        </li>)
+        </li>);
         return (
             <div>
                 <h1>Article list</h1>
@@ -33,4 +28,4 @@ class ArticleList extends Component {
     }
 }
 
-export default ArticleList
+export default openArticle(ArticleList)
